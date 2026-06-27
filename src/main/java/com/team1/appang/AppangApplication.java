6d8fe544d96b083efc95cf8dde6294e99a156a52
Seungheon Cha <tok9663@inu.ac.kr>
2026-06-27 17:07:09 +0900

    백엔드 레포 설정

---
+++ src/main/java/com/team1/appang/AppangApplication.java
@@ -0,0 +1,13 @@
+package com.team1.appang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/appang/AppangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.appang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
