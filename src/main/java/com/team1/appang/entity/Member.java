80c920caa2f7a515e3b5378c2186075d014399da
Seungheon Cha <tok9663@inu.ac.kr>
2026-07-05 18:46:01 +0900

    멤버 엔티티 작성

--- src/main/java/com/team1/appang/entity/Member.java
+++ src/main/java/com/team1/appang/entity/Member.java
@@ -1,4 +1,27 @@
 package com.team1.appang.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phoneNum;
 }
